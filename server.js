9d4a2cdb237547d347305ae4d666475f52604191
YGold-Dev <tmxpvksh0816@gmail.com>
2025-11-19 18:26:49 +0900

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
   res.send('Server working');
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
   console.log(`Server on http://localhost:${PORT}`);
 });
